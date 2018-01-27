7c61c30d9b3ae5683b9b76638923a512792b250f
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2018-01-27 11:33:28 +0000

    Initial commmit

---
+++ app/src/main/java/example/codeclan/com/todolist/TaskListActivity.java
@@ -0,0 +1,13 @@
+package example.codeclan.com.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaskListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.task_list_activity);
+    }
+}
