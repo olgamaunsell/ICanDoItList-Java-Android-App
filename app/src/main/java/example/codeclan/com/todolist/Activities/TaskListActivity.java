0746b45202c4bb055e3471c5abe3b7faa2d0a54a
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2018-01-29 15:38:09 +0000

    TaskListActivity updated to use Shared Prefs to populate ListView

--- app/src/main/java/example/codeclan/com/todolist/Activities/TaskListActivity.java
+++ app/src/main/java/example/codeclan/com/todolist/Activities/TaskListActivity.java
@@ -1,6 +1,8 @@
 package example.codeclan.com.todolist.Activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -9,27 +11,45 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
 
 import example.codeclan.com.todolist.Adapters.TaskListAdapter;
 import example.codeclan.com.todolist.Models.Task;
-import example.codeclan.com.todolist.Models.TaskList;
 import example.codeclan.com.todolist.R;
 
 public class TaskListActivity extends AppCompatActivity {
+
+    TextView newTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.task_list_activity);
 
-        ArrayList<Task> taskList = new TaskList().getList();
+        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE );
+//        JSON STRING
 
-        TaskListAdapter taskListAdapter = new TaskListAdapter(this, taskList);
+        String currentlySavedTasks = sharedPref.getString("AllTasks", new ArrayList<Task>().toString());
+        Log.d("All tasks", currentlySavedTasks);
+
+//        gson setup
+
+        Gson gson = new Gson();
+        TypeToken<ArrayList<Task>> taskArrayListToken = new TypeToken<ArrayList<Task>>(){};
+
+//        USING the JSON string to put into GSON ARRAYLIST
+        ArrayList<Task> tasksToShow = gson.fromJson(currentlySavedTasks, taskArrayListToken.getType());
+
+        TaskListAdapter taskListAdapter = new TaskListAdapter(this, tasksToShow);
 
         ListView listView = findViewById(R.id.task_list_view);
         listView.setAdapter(taskListAdapter);
+
     }
 
     @Override
